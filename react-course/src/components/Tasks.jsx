2095c883dac464186e3a16e4e580dbb9cc580cef
teodwro <teogreselle@gmail.com>
2025-03-05 15:10:21 -0300

    commit

--- react-course/src/components/Tasks.jsx
+++ react-course/src/components/Tasks.jsx
@@ -1,8 +1,20 @@
+import { ChevronRightIcon } from "lucide-react"
+
 function Tasks (props) {
     return (
         <ul className="space-y-4 p-6 bg-slate-200 rounded-mg shadow">
             {props.tasks.map((task) => (
-                <li key={task.id} className="bg-slate-400 text-white p-2 rounded-md">{task.title}</li>
+                <li key={task.id} className="flex gap-2">
+                    <button 
+                    className="bg-slate-400 text-white p-2 rounded-md w-full text-left" 
+                    onClick={() => props.onTaskClick(task.id)}>
+                        {task.title}
+                        {task.isCompleted ? "Complete" : "Incomplete"}
+                    </button> 
+                    <button className="bg-slate-400 p-2 rounded-md text-white">
+                        <ChevronRightIcon/>
+                    </button>
+                </li>
             ))}
         </ul>
     )
